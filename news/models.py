fc5f62250498fd99fa685925a61be633196109ad
DStupenev <dimastupenev@mail.ru>
2021-08-15 20:22:47 +0300

    A couple of modifications have been added.

--- news/models.py
+++ news/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    article_title = models.CharField(max_length=200)
+    article_text = models.TextField()
+    publication_date = models.DateTimeField()
+
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    author_name = models.CharField(max_length=50)
+    comment_text = models.CharField(max_length=300)
