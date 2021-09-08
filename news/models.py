f191a7b8d48b60196a25a53a27e2b9b00cc94e56
DStupenev <dimastupenev@mail.ru>
2021-09-08 23:48:07 +0300

    Some updates

--- news/models.py
+++ news/models.py
@@ -8,7 +8,7 @@ class News(models.Model):
     update_at = models.DateTimeField(auto_now=True, verbose_name='Редактировано')
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name='Фото', blank=True)
     is_published = models.BooleanField(default=True, verbose_name='Загружено?')
-    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name='Категория')
 
     def __str__(self):
         return self.title
@@ -20,7 +20,10 @@ class News(models.Model):
 
 
 class Category(models.Model):
-    title = models.CharField(max_length=150, db_index=True, verbose_name='Название категории')
+    title = models.CharField(max_length=150, db_index=True, verbose_name='Название категории', blank=True)
+
+    def __str__(self):
+        return self.title
 
     class Meta:
         verbose_name = 'Категория'
